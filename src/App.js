d4e855cb830f69545c20e32ab325b317ac604097
Davison Robie <AHDR@davisonsmacbook.attlocal.net>
2021-01-26 21:21:30 -0500

    Created Functional Components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Footer from "./components/challenges/day01/Footer.js"
+import TwoTruths from "./components/challenges/day01/TwoTruths.js"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <TwoTruths />
+     <Footer />
     </div>
   );
 }
---
+++ src/components/challenges/day01/TwoTruths.js
@@ -0,0 +1,15 @@
+const TwoTruths = () => {
+    return (
+        <div>
+            <h1>Davison Robie</h1>
+            <p>I love music, cooking, and some tech!</p>
+            <ul>
+                <li>Truth</li>
+                <li>Lie</li>
+                <li>Truth</li>
+            </ul>
+        </div>
+    )
+  }
+  
+  export default TwoTruths;
